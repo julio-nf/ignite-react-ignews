05378afda404add578d70c7dec8f30256562b036
Julio Novais <julio.novais@outlook.com>
2021-03-31 22:40:46 -0300

    feat: added header and home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
-        <title>início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get Access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
